d825f4e75268c13d1d2bf7158df0732db03a6a28
adrianrachmn <203040103@mail.unpas.ac.id>
2022-10-06 17:43:57 +0700

    constructor

--- oop/constructor.php
+++ oop/constructor.php
@@ -21,7 +21,7 @@ class Produk {
 
 $produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
 $produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000);
-$produk3 = new Produk("Dragon BalL");
+$produk3 = new Produk("Dragon Ball");
 
 echo "Komik : " . $produk1->getLabel();
 echo "<br>";
